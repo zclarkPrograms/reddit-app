016ce156b0e21c92401b23e6e73acf5575f3562b
zclarkPrograms <Zachary.Clark323@gmail.com>
2023-04-04 01:20:09 -0400

    removed commented code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,4 @@
-<!--<script>
-	import Nav from './nav.svelte';
-
-	const handleFormSubmit = (/** @type {{ detail: any; }} */ event) => {
-		const subreddit = event.detail;
-
-		console.log(subreddit);
-	};
-</script>
--->
-
 <div class="app">
-	<!--<Nav on:formSubmit={handleFormSubmit} />-->
-
 	<main>
 		<slot />
 	</main>
